f52b25718f3386f098933624b573c30966dcbc50
LiulietLee <lee.liuliet@hotmail.com>
2017-10-07 16:15:41 +0800

    hide rate row

---
+++ Pick Color/MenuCell.swift
@@ -0,0 +1,26 @@
+//
+//  MenuCell.swift
+//  Pick Color
+//
+//  Created by Liuliet.Lee on 7/10/2017.
+//  Copyright © 2017 Liuliet.Lee. All rights reserved.
+//
+
+import UIKit
+
+class MenuCell: MKTableViewCell {
+
+    @IBOutlet weak var title: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Pick Color/MenuTableViewController.swift
+++ Pick Color/MenuTableViewController.swift
@@ -10,15 +10,40 @@ import UIKit
 
 class MenuTableViewController: UITableViewController {
 
+    fileprivate var menuList: [(title: String, id: String)] = [
+        ("Pick Color", "picker"),
+        ("Favorite", "favorites"),
+        ("Author", "author"),
+        ("Rate PickColor", "rate"),
+        ("Source Code", "code")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return menuList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: menuList[indexPath.row].id, for: indexPath) as! MenuCell
+        
+        cell.title?.text = menuList[indexPath.row].title
+        
+        return cell
+    }
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if indexPath.row == 4 {
-            if #available(iOS 10.3, *) {
-                return
-            }
+        if #available(iOS 10.3, *) {
+            return
+        }
+        
+        if indexPath.row == 3 {
             guard let url = URL(string : "itms-apps://itunes.apple.com/app/id1205136568") else {
                 print("wtf?!")
                 return
@@ -31,9 +56,9 @@ class MenuTableViewController: UITableViewController {
         }
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        if #available(iOS 10.3, *) {
-            tableView.cellForRow(at: IndexPath(row: 4, section: 0))?.isHidden = true
+    override func viewWillAppear(_ animated: Bool) {
+        if #available(iOS 10.3, *), menuList[3].id == "rate" {
+            menuList.remove(at: 3)
         }
     }
     
